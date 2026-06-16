af8ad61d2d8e8877d409bced73c57e25db9d371f
heloisa <heloisa.f.santos10@aluno.senai.br>
2026-06-16 16:39:31 -0300

    criado alteração

--- js/scripst_repeticoes.js
+++ js/scripst_repeticoes.js
@@ -3,4 +3,10 @@ const divfor = document.querySelector('#div-for')
 
 for (i = 0;i <10; i++) { 
     console.log(i, 'Amor')
+    divfor.innerHTML += `${i} - Amor <br> ` 
 }
+
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector ('#div-cont-acum')
