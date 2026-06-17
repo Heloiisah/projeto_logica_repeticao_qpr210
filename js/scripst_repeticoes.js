3acfdb8c3dd995035c842862d7714e6bbf519d6e
Heloisa <heloisa.f.santos10@aluno.senai.br>
2026-06-17 16:11:59 -0300

    alteração

--- js/scripst_repeticoes.js
+++ js/scripst_repeticoes.js
@@ -70,3 +70,7 @@ for(let elemento of presentes){
  
 //FOREACH
 const divForeach = document.querySelector('#div-foreach')
+
+presentes.forEach((elemento, posicao)=> {
+    divForeach.innerHTML += `${posicao} - ${elemento}<br>`
+})
