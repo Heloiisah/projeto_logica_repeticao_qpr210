b96fb01bc7e975d90872ecff76a4dc0b8843d8cf
heloisa <heloia.f.santos10@aluno.senai.br>
2026-06-19 14:04:58 -0300

    alterar arquivo

--- js/scripts_funcoes.js
+++ js/scripts_funcoes.js
@@ -16,3 +16,9 @@ function somaNumeros(num1, num2) {
 }
 
 console.log(' As somas dos números ' , somaNumeros (10, 58))
+
+const multiplicacao = function(num1, num2){
+    return num1 * num2
+}
+
+console.log(multiplicacao(64, 5))
