3aed6041fd12d1fe4e4cb00e44ff8cf23d1b8635
heloisa <heloia.f.santos10@aluno.senai.br>
2026-06-19 14:02:27 -0300

    alterar arquivo

---
+++ js/scripts_funcoes.js
@@ -0,0 +1,18 @@
+
+function mensagem() {
+    console.log('Uma mensagem')
+}
+
+mensagem()
+
+function mensagemLinda(txt) {
+    console.log(txt)
+}
+
+mensagemLinda('Brasil vai da um pau no Haiti')
+
+function somaNumeros(num1, num2) {
+    return num1 + num2
+}
+
+console.log(' As somas dos números ' , somaNumeros (10, 58))
